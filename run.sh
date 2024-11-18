ffce0054c76af4f0f308597862c8b94471dab5fe
Nas Code <xiuxiu10201@gmail.com>
2024-11-18 15:27:46 +0800

    update

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ export DISPLAY=:115
 openbox &
 pcmanfm --desktop &
 tint2 &
-x11vnc -clipbell -display :115 -passwd ${PASSWORD} -listen 127.0.0.1 -nopw -forever -rfbport 1152 &
+x11vnc -display :115 -passwd ${PASSWORD} -listen 127.0.0.1 -nopw -forever -rfbport 1152 &
 sleep 3
 /usr/local/115Browser/115.sh
 websockify --web /usr/share/novnc 1150 localhost:1152
